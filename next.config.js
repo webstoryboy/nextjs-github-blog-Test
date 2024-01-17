69bd306eabb46f01e8483088cd84bb85025a44ba
websfonts <webstupids@gmail.com>
2024-01-17 11:45:10 +0900

    dd1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/nextjs-github-blog-Test' : '',
     output: 'export',
     distDir: 'dist',
     images: {
